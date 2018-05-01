854bde5818a19e6c003261efb460be703b1fd7ae
KOHEI OTSUKA <kochizufan@gmail.com>
2018-05-01 14:43:17 +0900

    最新の調査結果を適用

--- selenium_server.bat
+++ selenium_server.bat
@@ -1,1 +1,1 @@
-start java -jar C:\selenium-server-standalone-2.53.1.jar -Dwebdriver.chrome.driver=C:\chromedriver\chromedriver.exe
+start java -Dwebdriver.chrome.driver=C:\chromedriver\chromedriver.exe -jar C:\selenium-server-standalone-3.8.1.jar
