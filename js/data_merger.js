6c52bea975cea056ae3381e461eacd56c957621b
Kohei Otsuka <kochizufan@gmail.com>
2020-06-21 02:33:19 +0900

    jizo_project.geojson生成

---
+++ js/data_merger.js
@@ -0,0 +1,31 @@
+const fs = require('fs-extra');
+
+const pois = fs.readJsonSync('../pois.geojson');
+const files = fs.readJsonSync('../files.geojson');
+const refs = fs.readJsonSync('../refs.geojson');
+
+const refsBuffer = {};
+refs.features.map((curr) => {
+    if (!refsBuffer[curr.properties.poiid]) {
+        refsBuffer[curr.properties.poiid] = [];
+    }
+    refsBuffer[curr.properties.poiid].push(curr);
+});
+const filesBuffer = {};
+files.features.map((curr) => {
+    if (!filesBuffer[curr.properties.poiid]) {
+        filesBuffer[curr.properties.poiid] = [];
+    }
+    filesBuffer[curr.properties.poiid].push(curr);
+});
+pois.features.map((poi) => {
+    if (refsBuffer[poi.properties.fid]) {
+        poi.properties.refs = refsBuffer[poi.properties.fid];
+    }
+    if (filesBuffer[poi.properties.fid]) {
+        poi.properties.files = filesBuffer[poi.properties.fid];
+    }
+});
+fs.writeJsonSync('../jizo_project.geojson', pois, {
+    spaces: '  '
+});
