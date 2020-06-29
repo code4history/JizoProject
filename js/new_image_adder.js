8a1fdbc05d4203f016f10550a22dd3d1d61ad7fd
OHTSUKA Ko-hei <kochizufan@gmail.com>
2020-06-29 23:34:04 +0900

    新画像追加スクリプトもサムネイル対応

--- js/new_image_adder.js
+++ js/new_image_adder.js
@@ -1,5 +1,6 @@
 const ExifImage = require('exif').ExifImage;
 const fs = require('fs-extra');
+const sharp = require('sharp');
 
 const files = fs.readJsonSync('../files.geojson');
 const new_files = Object.assign({}, files);
@@ -38,40 +39,80 @@ folders.map((folder) => {
 console.log(fileNew);
 
 const promises = fileNew.map((curr) => {
-    return new Promise((resolve, reject) => {
-        const path = `.${curr.path}`;
-        try {
-            new ExifImage({ image : path }, function (error, exifData) {
-                if (error) {
-                    console.log('Error 1: ' + error.message + path);
-                    reject();
-                    return;
-                } else {
-                    const date = exifData.exif.DateTimeOriginal.replace(/^(\d{4}):(\d{2}):(\d{2}) /, "$1/$2/$3 ");
-                    const author = 'Kohei Otsuka';
-                    resolve({
-                        "type": "Feature",
-                        "properties": {
-                            "fid": null,
-                            "poiid": curr.poiid,
-                            "description": curr.description,
-                            "path": curr.path,
-                            "date": date,
-                            "author": author
-                        },
-                        "geometry": null
+    const path = curr.path;
+    const mid_thumb = path.replace('./images', './mid_thumbs');
+    const small_thumb = path.replace('./images', './small_thumbs');
+    return Promise.all([
+        new Promise((resolve, reject) => {
+            try {
+                new ExifImage({ image : `.${path}` }, function (error, exifData) {
+                    if (error) {
+                        console.log('Error 1: ' + error.message + path);
+                        reject();
+                        return;
+                    } else {
+                        const date = exifData.exif.DateTimeOriginal.replace(/^(\d{4}):(\d{2}):(\d{2}) /, "$1/$2/$3 ");
+                        const author = 'Kohei Otsuka';
+                        resolve({
+                            "type": "Feature",
+                            "properties": {
+                                "fid": null,
+                                "poiid": curr.poiid,
+                                "description": curr.description,
+                                "path": curr.path,
+                                "date": date,
+                                "author": author,
+                                "mid_thumbnail": mid_thumb,
+                                "small_thumbnail": small_thumb
+                            },
+                            "geometry": null
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log('Error 2: ' + error.message);
+                reject();
+            }
+        }),
+        new Promise((resolve) => {
+            try {
+                fs.statSync(`.${mid_thumb}`);
+            } catch (e) {
+                fs.ensureFileSync(`.${mid_thumb}`);
+                sharp(`.${path}`)
+                    .resize(800, 800, {
+                        kernel: sharp.kernel.nearest,
+                        fit: sharp.fit.inside
+                    })
+                    .toFile(`.${mid_thumb}`)
+                    .then(() => {
+                        curr.mid_thumbnail = mid_thumb;
+                        resolve();
                     });
-                }
-            });
-        } catch (error) {
-            console.log('Error 2: ' + error.message);
-            reject();
-        }
-    });
+            };
+        }),
+        new Promise((resolve) => {
+            try {
+                fs.statSync(`.${small_thumb}`);
+            } catch (e) {
+                fs.ensureFileSync(`.${small_thumb}`);
+                sharp(`.${path}`)
+                    .resize(200, 200, {
+                        kernel: sharp.kernel.nearest,
+                        fit: sharp.fit.inside
+                    })
+                    .toFile(`.${small_thumb}`)
+                    .then(() => {
+                        curr.small_thumbnail = small_thumb;
+                        resolve();
+                    });
+            };
+        })
+    ]);
 });
 
 Promise.all(promises).then((features) => {
-    new_files.features = features;
+    new_files.features = features.map(feature => feature[0]);
     fs.writeJsonSync('../files_new.geojson', new_files, {
         spaces: '  '
     });
