32d727a212a972a5e7a33e508adcf95aaab87b35
Kohei Otsuka <kochizufan@gmail.com>
2022-01-13 03:19:49 +0900

    Update swrvice worker

--- sw.js
+++ sw.js
@@ -13,42 +13,49 @@ workbox.navigationPreload.enable();
 // ------------------  runtime caching starts ---------------------
 // frequently updated resources
 workbox.routing.registerRoute(
-    new RegExp('jizo_project.geojson'),
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'geojson_data',
-    }),
-    'GET'
+  new RegExp('jizo_project.geojson'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'geojson_data',
+  }),
+  'GET'
 );
 
 // splash icon images
 workbox.routing.registerRoute(
-    new RegExp('(?:icons|assets|images|mid_thumbs|small_thumbs)/.*'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'icons',
-        maxEntries: 50
-    })
+  new RegExp('(?:icons|assets|images|mid_thumbs|small_thumbs)/.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'icons',
+    maxEntries: 50
+  })
 );
 
 // manifest
 workbox.routing.registerRoute(
-    new RegExp('manifest.json'),
-    new workbox.strategies.StaleWhileRevalidate()
+  new RegExp('manifest.json'),
+  new workbox.strategies.StaleWhileRevalidate()
 );
 
 // ------------------  precaching the assets ---------------------
 workbox.precaching.precacheAndRoute([
-    // html
-    'index.html',
-    // js
-    'https://unpkg.com/leaflet@1.6.0/dist/leaflet-src.js',
-    'assets/L.Control.Locate.js',
-    'assets/geojson-popup.js',
-    // css
-    'https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css',
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css',
-    'assets/L.Control.Locate.min.css',
-    'assets/style.css',
-    // other
-    'manifest.json'
+  // html
+  {'revision':'20220113', 'url':'index.html'},
+  // js
+  {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet-src.js'},
+  {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'},
+  {'revision':'20220113', 'url':'assets/leaflet-mapbox-gl.js'},
+  {'revision':'20220113', 'url':'https://unpkg.com/quyuanjs@0.0.7/dist/quyuan.min.js'},
+  {'revision':'20220113', 'url':'assets/popup_template.js'},
+  {'revision':'20220113', 'url':'assets/icon_template.js'},
+  {'revision':'20220113', 'url':'assets/twitter_Intent_url.js'},
+  {'revision':'20220113', 'url':'assets/oms.min.js'},
+  {'revision':'20220113', 'url':'assets/L.Control.Locate.js'},
+  // css
+  {'revision':'20220113', 'url':'https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css'},
+  {'revision':'20220113', 'url':'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'},
+  {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css'},
+  {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css'},
+  {'revision':'20220113', 'url':'assets/L.Control.Locate.min.css'},
+  {'revision':'20220113', 'url':'assets/style.css'},
+  // other
+  {'revision':'20220113', 'url':'manifest.json'}
 ]);
