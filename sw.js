baefa82c6d7bc0cce91d9c3fc6d2f30f69cb538e
Kohei Otsuka <kochizufan@gmail.com>
2022-01-16 16:53:19 +0900

    JS,css場所変更

--- sw.js
+++ sw.js
@@ -38,24 +38,24 @@ workbox.routing.registerRoute(
 // ------------------  precaching the assets ---------------------
 workbox.precaching.precacheAndRoute([
   // html
-  {'revision':'20220113', 'url':'index.html'},
+  {'revision':'20220116', 'url':'index.html'},
   // js
   {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet-src.js'},
   {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'},
-  {'revision':'20220113', 'url':'assets/leaflet-mapbox-gl.js'},
+  {'revision':'20220116', 'url':'src/leaflet-mapbox-gl.js'},
   {'revision':'20220113', 'url':'https://unpkg.com/quyuanjs@0.0.7/dist/quyuan.min.js'},
-  {'revision':'20220113', 'url':'assets/popup_template.js'},
-  {'revision':'20220113', 'url':'assets/icon_template.js'},
-  {'revision':'20220113', 'url':'assets/twitter_Intent_url.js'},
-  {'revision':'20220113', 'url':'assets/oms.min.js'},
-  {'revision':'20220113', 'url':'assets/L.Control.Locate.js'},
+  {'revision':'20220116', 'url':'src/popup_template.js'},
+  {'revision':'20220116', 'url':'src/icon_template.js'},
+  {'revision':'20220116', 'url':'src/twitter_Intent_url.js'},
+  {'revision':'20220116', 'url':'src/oms.min.js'},
+  {'revision':'20220116', 'url':'src/L.Control.Locate.js'},
   // css
   {'revision':'20220113', 'url':'https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css'},
   {'revision':'20220113', 'url':'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'},
   {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css'},
   {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css'},
-  {'revision':'20220113', 'url':'assets/L.Control.Locate.min.css'},
-  {'revision':'20220113', 'url':'assets/style.css'},
+  {'revision':'20220116', 'url':'src/L.Control.Locate.min.css'},
+  {'revision':'20220116', 'url':'src/style.css'},
   // other
   {'revision':'20220113', 'url':'manifest.json'}
 ]);
