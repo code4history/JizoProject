024112498c8814e72accab1175fbbfd92bf500a1
Kohei Otsuka <kochizufan@gmail.com>
2022-02-10 02:12:05 +0900

    Service worker更新

--- sw.js
+++ sw.js
@@ -38,18 +38,18 @@ workbox.routing.registerRoute(
 // ------------------  precaching the assets ---------------------
 workbox.precaching.precacheAndRoute([
   // html
-  {'revision':'2022011602', 'url':'index.html'},
+  {'revision':'20220210', 'url':'index.html'},
   // js
   {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet-src.js'},
   {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.js'},
   {'revision':'20220116', 'url':'src/leaflet-mapbox-gl.js'},
-  {'revision':'20220113', 'url':'https://unpkg.com/quyuanjs@0.0.7/dist/quyuan.min.js'},
-  {'revision':'20220124', 'url':'src/popup_template.js'},
+  {'revision':'20220210', 'url':'https://unpkg.com/quyuanjs@0.1.0/dist/quyuan.min.js'},
+  {'revision':'20220210', 'url':'src/popup_template.js'},
   {'revision':'2022011602', 'url':'src/icon_template.js'},
   {'revision':'20220116', 'url':'src/twitter_Intent_url.js'},
   {'revision':'20220116', 'url':'src/oms.min.js'},
   {'revision':'20220116', 'url':'src/L.Control.Locate.js'},
-  {'revision':'2022011602', 'url':'src/main.js'},
+  {'revision':'20220210', 'url':'src/main.js'},
   {'revision':'20220116', 'url':'src/twitter-text-3.1.0.min.js'},
   // css
   {'revision':'20220113', 'url':'https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css'},
@@ -57,7 +57,7 @@ workbox.precaching.precacheAndRoute([
   {'revision':'20220113', 'url':'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css'},
   {'revision':'20220113', 'url':'https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css'},
   {'revision':'20220116', 'url':'src/L.Control.Locate.min.css'},
-  {'revision':'20220123', 'url':'src/style.css'},
+  {'revision':'20220210', 'url':'src/style.css'},
   // other
   {'revision':'20220113', 'url':'manifest.json'},
   // image
