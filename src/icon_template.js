54842d90b030328de3e2455d7b4dc32275a24bcc
Kohei Otsuka <kochizufan@gmail.com>
2022-09-17 21:50:08 +0900

    スキーム統一完了

--- src/icon_template.js
+++ src/icon_template.js
@@ -1,27 +1,39 @@
 let iconTemplate = "";
 
 iconTemplate = `
-{%- set iconUrl = "hotoke" -%}
-{%- set width = 29 -%}
+{%- set iconUrl = "sekihi" -%}
+{%- set width = 32 -%}
 {%- set height = 44 -%}
 
 {%- if type.match("地蔵") -%}
   {%- set iconUrl = "jizo" -%}
   {%- set width = 23 -%}
-{%- elif type.match("小祠") -%}
+{%- elif type.match("小祠") or type.match("石祠") -%}
   {%- set iconUrl = "jinja" -%}
   {%- set width = 35 -%}
+{%- elif type.match("その他仏像") or type.match("明王") or type.match("菩薩") or type.match("如来") -%}
+  {%- set iconUrl = "hotoke" -%}
+  {%- set width = 29 -%}
+{%- elif type.match("庚申") -%}
+  {%- set iconUrl = "sanzaru" -%}
+  {%- set width = 24 -%}
+{%- elif type.match("馬頭観音") -%}
+  {%- set iconUrl = "bato" -%}
+  {%- set width = 23 -%}
+{%- elif type.match("月待塔") or type.match("如意輪観音") -%}
+  {%- set iconUrl = "19ya" -%}
+  {%- set width = 32 -%}
 {%- endif -%}
 
-{%- if lost -%}
+{%- if status and status.match("消失") -%}
   {%- set iconUrl = iconUrl + "_sepia" -%}
   {%- set height = 36 -%}
   {%- set width = (width * 24) / 32 -%}
-{%- elif need_action -%}
+{%- elif need_action or (not confirmed) or contradiction -%}
   {%- set iconUrl = iconUrl + "_surprise" -%}
   {%- set height = 36 -%}
   {%- set width = (width * 24) / 32 -%}
 {%- endif -%}
 
-./assets/{{- iconUrl -}}.png,{{- width -}},{{- height -}}
+./assets/{{- iconUrl -}}.png,{{- width | round -}},{{- height -}}
 `;
--- src/popup_template.js
+++ src/popup_template.js
@@ -9,7 +9,7 @@ popupHtmlTemplate += "{% if koaza %} / {{ koaza }} {% endif %}";
 popupHtmlTemplate += "{% if detail_place %} / {{ detail_place }} {% endif %}";
 popupHtmlTemplate += `)</h2>`;
 
-popupHtmlTemplate += `<a class="report-link" href="javascript:void(0)" onclick="prepareEditMarker({{ fid }}, '{{ title }}');">修正提案をする</a><br>`;
+popupHtmlTemplate += `<a class="report-link" href="javascript:void(0)" onclick="prepareEditMarker({{ fid }}, '{{ name }}');">修正提案をする</a><br>`;
 popupHtmlTemplate += `<div class="report-form"></div>`;
 
 popupHtmlTemplate += `{% if type %} <b>種別:</b> {{ type }} <br> {% endif %}`;
@@ -22,19 +22,31 @@ popupHtmlTemplate += `{% if images.length > 0 %}
   </qy-swiper>
 {% endif %}`;
 
-popupHtmlTemplate += `{% if era %}<b>年代:</b> {{ era }}<br>{% endif %}`;
-popupHtmlTemplate += `{% if year %}<b>西暦:</b> {{ year }}<br>{% endif %}`;
-popupHtmlTemplate += `{% if japan_calendar %}<b>和暦:</b> {{ japan_calendar }}<br>{% endif %}`;
-popupHtmlTemplate += `{% if description %}         <b>記述:</b> {{ description | nl2br | safe }}<br> {% endif %}`;
-popupHtmlTemplate += `{% if note %}        <b>メモ:</b> {{ note | nl2br | safe }}<br> {% endif %}`;
-popupHtmlTemplate += `{% if inscription %}        <b>刻銘:</b> {{ inscription | nl2br | safe }}<br> {% endif %}`;
-popupHtmlTemplate += `<b>最終現地調査日:</b> {%if brushup %} {{ surveyed }} {% else %} 未調査 {% endif %}<br>`;
-popupHtmlTemplate += `{% if height %}<b>総高:</b> {{ height }}cm<br>{% endif %}`;
-popupHtmlTemplate += `{% if partial_height %}<b>部分高:</b> {{ partial_height }}cm<br>{% endif %}`;
-popupHtmlTemplate += `{% if width %}<b>総高:</b> {{ width }}cm<br>{% endif %}`;
-popupHtmlTemplate += `{% if depth %}<b>総高:</b> {{ depth }}cm<br>{% endif %}`;
-popupHtmlTemplate += `{% if lost %}        <b>現況:</b> 現存せず<br> {% endif %}`;
-popupHtmlTemplate += `{% if need_action %}        <b>アクション要:</b> {{ need_action | nl2br | safe }}<br> {% endif %}`;
+popupHtmlTemplate += `<b>年代:</b> {{ era }} {% if year %} ({{ year }}) {% endif %}<br>`;
+
+popupHtmlTemplate += `{% if shape %}         <b>形状:</b> {{ shape }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if reference_memo %}        <b>付帯情報:</b> {{ reference_memo | nl2br | safe }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if history %}        <b>来歴等:</b> {{ history | nl2br | safe }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if folklore %}        <b>伝承等:</b> {{ folklore | nl2br | safe }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if survey_memo %}        <b>調査時メモ:</b> {{ survey_memo | nl2br | safe }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if material %}      <b>材質:</b> {{ material }}<br> {% endif %}`;
+popupHtmlTemplate += `{% if height %}        <b>総高:</b> {{ height }}cm<br> {% endif %}`;
+popupHtmlTemplate += `{% if partial_height %} <b>部分高:</b> {{ partial_height }}cm<br> {% endif %}`;
+popupHtmlTemplate += `{% if width %}         <b>幅:</b> {{ width }}cm<br> {% endif %}`;
+popupHtmlTemplate += `{% if depth %}         <b>奥行:</b> {{ depth }}cm<br> {% endif %}`;
+popupHtmlTemplate += `{% if inscription %}   <b>刻銘:</b> {{ inscription | nl2br | safe }}<br> {% endif %}`;
+
+popupHtmlTemplate += `<b>最終現地調査日:</b> {%if confirmed %} {{ surveyed }} {% else %} 未調査 {% endif %}<br>`;
+
+popupHtmlTemplate += `<b>現況:</b>
+  {% if status == "消失" %} 喪失
+  {% elif status == "消失?" %} 喪失疑い
+  {% elif status == "新規発見" %} 新規追加(資料にない)
+  {% elif confirmed %} 現存
+  {% else %} 不明(未調査) {% endif %}<br>`;
+
+popupHtmlTemplate += `{% if need_action %}   <b>アクション要:</b> {{ need_action }} <br> {% endif %}`;
+popupHtmlTemplate += `{% if contradiction %} <b>資料の矛盾:</b> {{ contradiction }} <br> {% endif %}`;
 
 popupHtmlTemplate += `{% if books.length > 0 %}<b>言及資料:</b><br>`;
 popupHtmlTemplate += `<ul class="parent">
@@ -43,11 +55,12 @@ popupHtmlTemplate += `<ul class="parent">
       {% if book.anchor and book.anchor.match("^http") %}
         <a href="{{ book.anchor }}">{{ book.title }}</a> ({{ book.editor }})
       {% else %}
-        <b>{% if book.url %}<a href="{{ book.url }}">{% endif %}{{ book.title }}{% if book.url %}</a>{% endif %}</b> ({% if book.editor %}{{ book.editor }}{% else %}{{ book.publisher }}{% endif %}{% if book.published_at %}, {{ book.published_at }}{% endif %}){% if book.anchor %}: {{ book.anchor }}ページ{% endif %}
+        <b>{% if book.url %}<a href="{{ book.url }}">{% endif %}{{ book.title }}{% if book.url %}</a>{% endif %}</b> 
+        ({% if book.editor %}{{ book.editor }}{% else %}{{ book.publisher }}{% endif %}{% if book.published_at %}, {{ book.published_at }}{% endif %})
+        {% if book.anchor %}: {{ book.anchor }}{% if book.type == "book" %}ページ{% elif book.type == "journal" %}号{% endif %}{% endif %}
       {% endif %}
     </li>    
   {% endfor %}
 </ul>{% endif %}`;
-
 
 popupHtmlTemplate += `</div>`;
